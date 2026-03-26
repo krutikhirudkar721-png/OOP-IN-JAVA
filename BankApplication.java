69181af495a6fa63927b36289ac88fc3544e0bde
Krutik Hirudkar <krutikhirudkar721@gmail.com>
2026-03-26 14:00:33 +0530

    Create BankApplication.java

---
+++ BankApplication.java
@@ -0,0 +1,17 @@
+// Use throws and handle exception in main
+
+class BankApplication {
+
+    static void process() throws Exception {
+        throw new Exception("Transaction failed");
+    }
+
+    public static void main(String[] args) {
+        try {
+            process();
+            System.out.println("Transaction processed");
+        } catch (Exception e) {
+            System.out.println("Exception handled in main");
+        }
+    }
+}
