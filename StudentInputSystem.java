eb36f2501d928720048121f0e9bdfaffcf276df9
Krutik Hirudkar <krutikhirudkar721@gmail.com>
2026-03-26 13:59:17 +0530

    Create StudentInputSystem.java

---
+++ StudentInputSystem.java
@@ -0,0 +1,18 @@
+// Handle invalid input using multiple catch blocks
+
+import java.util.*;
+
+class StudentInputSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            int age = sc.nextInt();
+            System.out.println("Student age is: " + age);
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input");
+        } catch (ArithmeticException e) {
+            System.out.println("Arithmetic error");
+        }
+    }
+}
