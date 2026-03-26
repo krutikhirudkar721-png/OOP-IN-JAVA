ee244f645857d322b65cbe86189577f6b7d903fc
Krutik Hirudkar <krutikhirudkar721@gmail.com>
2026-03-26 13:53:32 +0530

    Create WordCounter.java

---
+++ WordCounter.java
@@ -0,0 +1,14 @@
+// Count words using StringTokenizer
+
+import java.util.*;
+
+class WordCounter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String sentence = sc.nextLine();
+        StringTokenizer st = new StringTokenizer(sentence);
+
+        System.out.println("Word Count: " + st.countTokens());
+    }
+}
