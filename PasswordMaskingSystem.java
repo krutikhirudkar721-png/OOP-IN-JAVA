0bbaadd90903ee9f537a3cb0aa0f812b8ec3589e
Krutik Hirudkar <krutikhirudkar721@gmail.com>
2026-03-26 13:54:53 +0530

    Create PasswordMaskingSystem.java

---
+++ PasswordMaskingSystem.java
@@ -0,0 +1,13 @@
+// Mask password except last 2 characters
+
+import java.util.Scanner;
+
+class PasswordMaskingSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String pass = sc.nextLine();
+
+        String masked = pass.replaceAll(".(?=.{2})", "*");
+        System.out.println(masked);
+    }
+}
