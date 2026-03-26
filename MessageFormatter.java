7015849cf8c9afbc875313c23735c6d5cb7a9a3d
Krutik Hirudkar <krutikhirudkar721@gmail.com>
2026-03-26 13:52:50 +0530

    Create MessageFormatter.java

---
+++ MessageFormatter.java
@@ -0,0 +1,15 @@
+// Concatenate first and last name
+
+import java.util.Scanner;
+
+class MessageFormatter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String first = sc.nextLine();
+        String last = sc.nextLine();
+
+        String full = first + " " + last;
+        System.out.println("Full Name: " + full);
+    }
+}
