9406fc612195c04fabd0e7d49996a68b86ed92d9
Krutik Hirudkar <krutikhirudkar721@gmail.com>
2026-03-26 14:01:45 +0530

    Create RailwayReservation.java

---
+++ RailwayReservation.java
@@ -0,0 +1,23 @@
+// Synchronization to avoid double booking
+
+class RailwayReservation {
+    int seats = 1;
+
+    synchronized void bookTicket() {
+        if (seats > 0) {
+            System.out.println("Ticket booked successfully");
+            seats--;
+        } else {
+            System.out.println("No tickets available");
+        }
+    }
+
+    public static void main(String[] args) {
+        RailwayReservation obj = new RailwayReservation();
+
+        Runnable task = () -> obj.bookTicket();
+
+        new Thread(task).start();
+        new Thread(task).start();
+    }
+}
