99492cd2e91cd5e7c1af42b6e892428a39bf4da1
Krutik Hirudkar <krutikhirudkar721@gmail.com>
2026-04-01 21:42:19 +0530

    Update DivisionCalculator.java

--- DivisionCalculator.java
+++ DivisionCalculator.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 class DivisionCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         try {
             int a = sc.nextInt();
             int b = sc.nextInt();
